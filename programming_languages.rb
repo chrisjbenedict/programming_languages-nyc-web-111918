0bb182adbdd0ce826680bbd2ec06b76c8ebd409f
Christopher Benedict <chrisjbenedict@gmail.com>
2018-10-30 02:40:53 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -26,11 +26,18 @@ languages = {
     :javascript => {
       :type => "interpreted"
     }
- 
+
   }
 }
 
 def reformat_languages(languages)
-  # your code here
-  
-end
+   new_languages = {}
+   languages.each do |type, hash|
+     hash.each do |name, attributes|
+       new_languages[name] ||= attributes
+       new_languages[name][:style] ||= []
+       new_languages[name][:style] << type
+     end
+   end
+   new_languages
+ end
